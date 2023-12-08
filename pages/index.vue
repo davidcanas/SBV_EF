d2c48cd2882e347ea972d0545355d037fbb8ad79
davidcanas <srd4dev@gmail.com>
2023-12-08 14:50:05 +0000

    ✨ | Release, first commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <iframe loading="lazy" src="https://www.canva.com/design/DAF2Yqx3bMc/view?embed" allowfullscreen=true
+      allow="fullscreen">
+    </iframe>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #282b30;
+}
+
+.container {
+  background-color: #282b30;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+
+iframe {
+  width: 85%;
+  ;
+  height: 85%;
+  margin-bottom: 0;
+  border: 8px solid whitesmoke;
+  box-shadow: 10px 20px #5539cc;
+  border-radius: 5px;
+}
+</style>
